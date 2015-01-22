8b93a5483e9ff7ea6c01692f4fa0e8c6e0fb48ea
Joshua Smith <joshsmith0419@gmail.com>
2015-01-21 18:51:18 -0800

    all the files to start the project

---
+++ Awesomenauts Files/js/resources.js
@@ -0,0 +1,29 @@
+game.resources = [
+
+	/* Graphics. 
+	 * @example
+	 * {name: "example", type:"image", src: "data/img/example.png"},
+	 */
+	{name: "background-tiles", type: "image", src: "data/img/background-tiles.png"},//show the background onto the website
+	{name: "meta-tiles", type: "image", src: "data/img/meta-tiles.png"},//Shows the meta tiles on the website
+	/* Atlases 
+	 * @example
+	 * {name: "example_tps", type: "tps", src: "data/img/example_tps.json"},
+	 */
+		
+	/* Maps. 
+	 * @example
+	 * {name: "example01", type: "tmx", src: "data/map/example01.tmx"},
+	 * {name: "example01", type: "tmx", src: "data/map/example01.json"},
+ 	 */
+ 	 {name: "level01", type: "tmx", src: "data/map/test.tmx"},//allows the level to load the game
+	/* Background music. 
+	 * @example
+	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
+	 */	
+
+	/* Sound effects. 
+	 * @example
+	 * {name: "example_sfx", type: "audio", src: "data/sfx/"}
+	 */
+];
