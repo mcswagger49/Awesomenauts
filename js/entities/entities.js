6f75f82b7bcb4827483596060359ef300ecf288a
Joshua Smith <joshsmith0419@gmail.com>
2015-01-29 18:51:18 -0800

    added a enemy player Video 19

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -204,7 +204,7 @@ game.EnemyCreep = me.Entity.extend({
 		this.health = 10;
 		this.alwaysUpdate = true;
 
-		this.setVelocity(3, 20);
+		this.body.setVelocity(3, 20);
 
 		this.type ="EnemyCreep";
 
@@ -214,5 +214,24 @@ game.EnemyCreep = me.Entity.extend({
 
 	update:function() {
 	
+	}
+});
+game.GameManager = Object.extend({
+	init: function(x, y, settings) {
+		this.now = new Date().getTime();
+		this.lastCreep = new Date().getTime();
+
+		this.alwaysUpdate = true;
+	},
+
+	update: function(){
+		this.now = new Date().getTime();
+
+		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)) {
+			this.lastCreep = this.now;
+			var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
+			me.game.world.addChild(creepe, 5);
+		}
+		return true;
 	}
 });
