8a732708a87eaa504fa41a22670a4d1d15e757ef
Joshua Smith <joshsmith0419@gmail.com>
2015-02-02 10:13:57 -0800

    creep hits player base Video 21

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -121,8 +121,7 @@ game.PlayerBaseEntity = me.Entity.extend({//launchs the playersbase to show
 		this.health = 10;
 		this.alwaysUpdate = true;
 		this.body.onCollision = this.onCollision.bind(this);
-		console.log("init");
-		this.type = "PlayerBaseEntity";
+		this.type = "PlayerBase";
 
 		this.renderable.addAnimation("idle", [0]);//makes the player orc to face the screen 
 		this.renderable.addAnimation("broken", [1]);	
@@ -138,6 +137,10 @@ update:function(delta) {
 
 	this._super(me.Entity, "update", [delta]);
 	return true;
+},
+
+loseHealth: function(damage){
+	this.health = this.health - damage;
 },
 
 onCollision: function(){
@@ -203,17 +206,50 @@ game.EnemyCreep = me.Entity.extend({//code for the enemycreep to be on webstie
 	}]);
 		this.health = 10;
 		this.alwaysUpdate = true;
-
+		//this.attcking lets us know if the enemy is hitting the base
+		this.attacking = false;
+		//keeps track of when our creep last attacked the base
+		this.lastAttacking = new Date().getTime();
+		//keeps track of the last time our creep hit anything
+		this.lastHit = new Date().getTime();
+		this.now = new Date().getTime();
 		this.body.setVelocity(3, 20);
 
 		this.type ="EnemyCreep";
-
+	
 		this.renderable.addAnimation("walk", [3, 4, 5], 80);
 		this.renderable.setCurrentAnimation("walk");	
 	},
 
-	update:function() {
-	
+	update:function(delta){//function to make creep move 
+		this.now = new Date().getTime();
+
+		this.body.vel.x -= this.body.accel.x * me.timer.tick;//makes creep spawn and move
+
+		me.collision.check(this, true, this.collideHandler.bind(this), true);//checking for collisions
+
+		this.body.update(delta);//updates constantly
+
+		this._super(me.Entity, "update", [delta]);//updates movement
+		return true;
+	},
+
+	collideHandler: function(response){//checking for collisions
+		if(response.b.type==='PlayerBase') {
+			this.attacking=true;
+			//this.lastAttacking=this.now;
+			this.body.vel.x = 0;
+			//keeps moving the creep to the right to main tain its position
+			this.pos.x = this.pos.x + 1;
+			//checks that it has been at least 1 second since this creep hit a base
+			if((this.now-this.lastHit >= 1000)) {
+				//updates the lastHit timer
+				this.lastHit = this.now;
+				//makes the player base call its loseHealth function
+				//da,age of 1
+				response.b.loseHealth(1);
+			}
+		}
 	}
 });
 game.GameManager = Object.extend({
