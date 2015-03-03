c75950f10571c9aba78e81721f3390d9b408283f
Joshua Smith <joshsmith0419@gmail.com>
2015-03-03 08:32:56 -0800

    fixed Re-fractor codes Video 34

--- index.php
+++ index.php
@@ -32,6 +32,7 @@
 		<script type="text/javascript" src="js/entities/PlayerBaseEntity.js"></script>
 		<script type="text/javascript" src="js/entities/GameManager.js"></script>
 		<script type="text/javascript" src="js/entities/EnemyCreep.js"></script>
+		<script type="text/javascript" src="js/entities/Gloop.js"></script>
 		<script type="text/javascript" src="js/entities/HUD.js"></script>
 
 		<script type="text/javascript" src="js/screens/title.js"></script>
--- js/entities/Gloop.js
+++ js/entities/Gloop.js
@@ -1,4 +1,3 @@
-//first try on intermidiate 
 game.Gloop = me.Entity.extend({//code for the enemycreep to be on webstie
 	init: function(x, y, settings) {
 		this._super(me.Entity, 'init', [x, y, {
@@ -45,3 +44,26 @@ game.Gloop = me.Entity.extend({//code for the enemycreep to be on webstie
 		this._super(me.Entity, "update", [delta]);//updates movement
 		return true;
 	},
+	collideHandler: function(response){//checking for collisions
+		if (response.b.type==='EnemyCreep'){
+			var xdif = this.pos.x - response.b.pos.x;
+
+			this.attacking=true;
+			//this.lastAttacking=this.now;
+			this.body.vel.x = 0;
+			//keeps moving the creep to the right to main tain its position
+			if (xdif>0){
+				this.pos.x = this.pos.x + 1;
+				this.body.vel.x = 0;
+			}
+			//checks that it has been at least 1 second since this creep hit a base
+			if((this.now-this.lastHit >= 1000) && xdif>0) {
+			//updates the lastHit timer
+			this.lastHit = this.now;
+			//makes the player call its loseHealth function
+			//damage of 1
+			response.b.loseHealth(game.data.enemyCreepAttack);
+			}
+		}
+	},
+});
