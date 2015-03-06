79c55c3fa0f662bd290338e0d083431ab9f9635a
Joshua Smith <joshsmith0419@gmail.com>
2015-03-06 08:48:07 -0800

    Just Testing Code Video 41

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -51,13 +51,13 @@ game.HeroDeathManager = Object.extend({
 game.ExperienceManager = Object.extend({
 	init: function(x, y, settings) {
 		this.alwaysUpdate = true;
-		this.gameOver = false;
+		this.gameover = false;
 	},
 
 	update: function(){
-		if (game.data.win === true && !this.gameOver) {
+		if (game.data.win === true && !this.gameover) {
 			this.gameOver(true);
-		}else if(game.data.win === false && !this.gameOver){
+		}else if(game.data.win === false && !this.gameover){
 			this.gameOver(false);
 		}
 
@@ -70,11 +70,10 @@ game.ExperienceManager = Object.extend({
 		}else{
 			game.data.exp += 1;
 		}
-
-		this.gameOver = true;
+		console.log(game.data.exp);
+		this.gameover = true;
 		me.save.exp = game.data.exp;
-	}	
-
+	}
 
 }); 
 
--- js/game.js
+++ js/game.js
@@ -49,6 +49,9 @@ var game = {
 	}
 	me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
 
+	console.log(game.data.exp);
+	console.log(game.data.exp2);
+
 	// Initialize the audio.
 	me.audio.init("mp3,ogg");
 
