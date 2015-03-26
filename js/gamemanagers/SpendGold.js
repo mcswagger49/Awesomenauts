9656b14a810c57aad48d8f19418ac2c28273e943
Joshua Smith <joshsmith0419@gmail.com>
2015-03-26 08:51:40 -0700

    RE-fractoring GameManagers

--- js/gamemanagers/SpendGold.js
+++ js/gamemanagers/SpendGold.js
@@ -1,81 +1,3 @@
-game.GameTimerManager = Object.extend({
-	init: function(x, y, settings) {
-		this.now = new Date().getTime();
-		this.lastCreep = new Date().getTime();
-		this.paused = false; 
-		this.alwaysUpdate = true;
-	},
-
-	update: function(){
-		this.now = new Date().getTime();
-		this.goldTimerCheck();
-		this.creepTimerCheck();
-
-
-		
-		return true;
-	},
-
-	goldTimerCheck: function(){
-		if(Math.round(this.now/1000)%20 ===0 && (this.now - this.lastCreep >= 1000)) {
-			game.data.gold += (game.data.exp1+1);
-			console.log("Current gold: " + game.data.gold);
-		}
-	},
-
-	creepTimerCheck: function(){
-		if(Math.round(this.now/1000)%10 ===0 && (this.now - this.lastCreep >= 1000)) {
-			this.lastCreep = this.now;
-			var creepe = me.pool.pull("EnemyCreep", 1000, 0, {});
-			me.game.world.addChild(creepe, 5);
-			var gloop = me.pool.pull("Gloop", 200, 0, {});
-			me.game.world.addChild(gloop, 5);
-		}
-	}
-});
-
-game.HeroDeathManager = Object.extend({
-	init: function(x, y, settings){
-		this.alwaysUpdate = true;
-	},
-
-	update: function(){
-		if(game.data.player.dead){
-			me.game.world.removeChild(game.data.player);
-			me.state.current().resetPlayer(10, 0);
-		}
-		return true;
-	}
-});
-
-game.ExperienceManager = Object.extend({
-	init: function(x, y, settings) {
-		this.alwaysUpdate = true;
-		this.gameover = false;
-	},
-
-	update: function(){
-		if (game.data.win === true && !this.gameover) {
-			this.gameOver(true);
-		}else if(game.data.win === false && !this.gameover){
-			this.gameOver(false);
-		}
-
-		return true;
-	},
-
-	gameOver: function(win){
-		if(win){
-			game.data.exp += 10;
-		}else{
-			game.data.exp += 1;
-		}
-		console.log(game.data.exp);
-		this.gameover = true;
-		me.save.exp = game.data.exp;
-	}
-}); 
-
 game.SpendGold = Object.extend({
  init: function(x, y, settings){
  		this.now = new Date().getTime();
@@ -208,7 +130,7 @@ makePurchase: function(skill){
 	if (skill === 1) {
 		game.data.gold -= ((game.data.skill1+1)*10);
 		game.data.skill1 += 1;
-		game.data.player.attack += 1; 
+		game.data.playerAttack += 1; 
 	}else if(skill ===2){
 		game.data.gold -= ((game.data.skill2+1)*10);
 		game.data.skill2 += 1;
