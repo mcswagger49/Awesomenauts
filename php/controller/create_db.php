7feb55ccb8ddedddb95234a44a6e0bc6ed9428ed
Joshua Smith <joshsmith0419@gmail.com>
2015-04-05 22:02:11 -0700

    Finishing Adding PHP

--- php/controller/create_db.php
+++ php/controller/create_db.php
@@ -1,29 +1,17 @@
 <?php
 	require_once(__DIR__ . "/../model/config.php"); //changed to config file because of the new Database class
 
-	$query = $_SESSION["connection"]->query("CREATE TABLE posts ("//create a table in local 
-		. "id int(11) NOT NULL AUTO_INCREMENT," //autoincrement is going to give it a name in localhost automaticlly
-		. "title varchar(255) NOT NULL,"//the title 
-		. "post text NOT NULL,"//posts the text on the website 
-		. "DateTime datetime NOT NULL,"//helps posts the datetime on the webpage.
-		. "PRIMARY KEY (id))");//the id in primary key 
-
-	if($query) {
-
-		echo "<p>Successfully create table: posts</p>";//shows or echos the text on the website
-}
-
-	else {
-
-		echo "<p>" . $_SESSION["connection"]->error. "</p>";
-}
-
 		$query = $_SESSION["connection"]->query("CREATE TABLE users("//where the database funciton going to be called
 			   .  "id int(11) NOT NULL AUTO_INCREMENT,"//increments the id automaticially
 			   .  "username varchar(30) NOT NULL," //stores the username 
 			   .  "email varchar(50) NOT NULL,"//stores the email 
 			   .  "password char(128) NOT NULL,"//stores the password
 			   .  "salt char(128) NOT NULL,"//supposed to stop hackers to get into the query code.
+			   .  "exp int(4),"
+			   .  "exp1 int(4),"
+			   .  "exp2 int(4),"
+			   .  "exp3 int(4),"
+			   .  "exp4 int(4),"
 			   .  "PRIMARY KEY (id))");//sets the primary key w/ id
 
 		if ($query) {
--- php/controller/login-user.php
+++ php/controller/login-user.php
@@ -24,8 +24,7 @@
 	  	  	$array["exp2"] = $row["exp2"];
 	  	  	$array["exp3"] = $row["exp3"];
 	  	  	$array["exp4"] = $row["exp4"];
-
-
+			$_SESSION["name"] = $username; 
 	  	  	echo json_encode($array);//Logged in correctly
 	  	  } else {
 	  	  	echo "<p>Invalid username and password</p>";//tells the user whats wrong but not actually teling them ;D
